4023cc3cb98d94a728a110e109d3e1f99ad47c18
michalrobertswd2017 <michalrob51@gmail.com>
2016-11-22 21:40:42 +0200

    Assignment 3

---
+++ assignments/class3/assets2/jquery_test.js
@@ -0,0 +1,20 @@
+
+
+    var myFooter = $("footer");
+    var mydiv = $(".div");
+
+    myFooter.on("mouseenter", function(){
+        mydiv.fadeIn();
+    }).on("mouseleave", function(){
+        mydiv.fadeOut();
+
+    })
+
+    ;
+
+
+
+
+
+
+}
