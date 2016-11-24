2002e2d3742dfaa59fd1f68ab8b201daaf0af9b7
michalrobertswd2017 <michalrob51@gmail.com>
2016-11-24 12:53:15 +0200

    assignment 3 CORRECTIONS

--- assignments/class3/assets2/jquery_test.js
+++ assignments/class3/assets2/jquery_test.js
@@ -1,16 +1,14 @@
-
-
+$(function(){
     var myFooter = $("footer");
     var mydiv = $(".div");
 
     myFooter.on("mouseenter", function(){
+        //alert("hi!");
         mydiv.fadeIn();
     }).on("mouseleave", function(){
         mydiv.fadeOut();
 
-    })
-
-    ;
+    });
 
 
-}
+});
