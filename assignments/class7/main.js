3f1a19f5641b69bdcf1f17be91fddef8e76b5b36
michalrobertswd2017 <michalrob51@gmail.com>
2016-12-21 00:38:51 +0200

    assignment 7

---
+++ assignments/class7/main.js
@@ -0,0 +1,22 @@
+$ (function) {
+
+    var h = "50%"
+    var s = "50%"
+
+    var $mySlider1 = #slider1;
+    var $mySlider1 = #slider2;
+
+    $mySlider1.on("mouseover", function(){
+        $(this).next().css("color",$(this).val());
+    })
+
+}
+
+$("#mycircle").css("background", "hsl(0, "+s+"%, "+l+"%)");
+
+    });
+
+   $("#mycircle").data("color","hsl(0, "+s+"%, "+l+"%)");
+});
+
+
