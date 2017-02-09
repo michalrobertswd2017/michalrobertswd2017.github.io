651cf456daf885d0580622f2e1c745ce3c408896
michalrobertswd2017 <michalrob51@gmail.com>
2017-02-09 09:30:59 +0200

    09/02/2017

---
+++ final-project/final/about.php
@@ -0,0 +1,10 @@
+ <? include("header.html"); ?>
+
+ <link href="mainabout.css" rel="stylesheet">
+    <img src="background_orange_splatter">
+    <body>
+        <main>Play therapy is a method of working with a child to help him solve the issues he is living with, through play sessions.</main>
+        <script src="...."></script>
+    </body>
+
+<? include("footer.html"); ?>
---
+++ final-project/final/home.php
@@ -0,0 +1,14 @@
+        <? include("header.html"); ?>
+
+    <main>
+        <img id="child1" src="therapy1.jpeg">
+        <img id="child2" src="therapy2.jpeg">
+
+        <h1> Rainbow Play Therapy is....</h1>
+
+        </main>
+
+
+         <? include("footer.html"); ?>
+
+
